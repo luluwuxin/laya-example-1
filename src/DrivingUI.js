4144a5f09264359097682ba17071022127bc04b1
mingxiang <xumingxiang@51hitech.com>
2018-01-18 14:44:50 +0800

    Move the WebSocket client out of a specific page.
    We have only one laya stage and on html page. Different laya pages will share the same
    websocket client instance.
    Remove anti-pattern code. Views register callbacks to model. Once model changes, Views
    get updated.

--- src/DrivingUI.js
+++ src/DrivingUI.js
@@ -1,17 +1,8 @@
-// MVC classes for the Open Scene Driving webpage.
+// Class for the Open Scene Driving webpage.
 //
-function DrivingUI()
+function DrivingUI(client)
 {
     DrivingUI.super(this);
-
-    // Model: A list of scenes and default settings.
-    this.scene_list = [];
-
-    // Model: The current scene in edit.
-    this.scene_now = undefined;
-
-    // Initialize WebSocket
-    this.initWS();
 
     // Initialize UI elements
     this.initSceneListUI();
@@ -20,58 +11,24 @@ function DrivingUI()
     this.initWeatherUI();
     this.initTrafficUI();
 
-    // Mock
-    var _this = this;
-    setTimeout(function () {
-        _this.setSceneList(_this.getMockSceneList());
-    }, 10);
+    // Model and WebSocket backend
+    this.client = client
+      .on("__init_scene_list", this, function () {
+          this.refreshPathUI();
+          this.refreshWeatherUI();
+          this.refreshTrafficUI();
+          this.refreshSceneListUI();
+      })
+      .on("scene_info", this, function () {
+          this.refreshPathUI();
+      })
+      .on("weather_info", this, function () {
+          this.refreshWeatherUI();
+      })
+      .on("traffic_info", this, function () {
+          this.refreshTrafficUI();
+      });
 }
-
-//
-// View
-//
-
-// Return a mock scene for testing.
-DrivingPageUI.prototype.getMockSceneList = function () {
-    return [
-        {
-            scene_info: {
-                scene: "IndistrialCity",
-                path: "default",
-            },
-            weather_info: {
-                temperature: 25,
-                time_of_day: "9:00",
-                rain_type: true,
-                snow_type: false,
-                fog_type: "HeaveFog",
-            },
-            traffic_info: {
-                car_density: 50,
-                pedestrain_density: 19,
-                car_irregularity: 29,
-            },
-        },
-        {
-            scene_info: {
-                scene: "Changning",
-                path: "default",
-            },
-            weather_info: {
-                temperature: -10,
-                time_of_day: "19:00",
-                rain_type: false,
-                snow_type: true,
-                fog_type: "",
-            },
-            traffic_info: {
-                car_density: 500,
-                pedestrain_density: 109,
-                car_irregularity: 209,
-            },
-        },
-    ];
-};
 
 // Init the scene list UI.
 DrivingPageUI.prototype.initSceneListUI = function () {
@@ -83,7 +40,7 @@ DrivingPageUI.prototype.initSceneListUI = function () {
     // Mouse events.
     this.m_uiSceneList.mouseHandler = new Handler(this, function (e, i) {
         if (e.type === Laya.Event.CLICK) {
-            this.setScene(i);
+            this.client.chooseScene(i);
         }
     });
 };
@@ -129,36 +86,39 @@ DrivingPageUI.prototype.initWeatherUI = function () {
         // Input: UI -> Model
         var data = parseFloat(e.text);
         if (isFinite(data)) {
-            this.scene_now.weather_info.temperature = data;
+            this.client.scene.weather_info.temperature = data;
         }
     });
     this.m_uiWeather_temperature.on(Laya.Event.BLUR, this, function (e) {
         // Blur: Model -> UI
-        this.refreshWeatherUI();
+        this.client.fire("weather_info");
     });
 
     this.m_uiWeather_timeOfDay.on(Laya.Event.INPUT, this, function (e) {
         // Input: UI -> Model
-        this.scene_now.weather_info.time_of_day = e.text;
+        var data = parseFloat(e.text);
+        if (isFinite(data)) {
+            this.client.scene.weather_info.time_of_day = data;
+        }
     });
     this.m_uiWeather_timeOfDay.on(Laya.Event.BLUR, this, function (e) {
         // Blur: Model -> UI
-        this.refreshWeatherUI();
+        this.client.fire("weather_info");
     });
 
     this.m_uiWeather_rainType.on(Laya.Event.CHANGE, this, function (e) {
         // Change: UI -> Model
-        this.scene_now.weather_info.rain_type = e.target.selectedIndex > 0;
+        this.client.scene.weather_info.rain_type = e.target.selectedIndex > 0;
     });
 
     this.m_uiWeather_snowType.on(Laya.Event.CHANGE, this, function (e) {
         // Change: UI -> Model
-        this.scene_now.weather_info.snow_type = e.target.selectedIndex > 0;
+        this.client.scene.weather_info.snow_type = e.target.selectedIndex > 0;
     });
 
     this.m_uiWeather_fogType.on(Laya.Event.CHANGE, this, function (e) {
         // Change: UI -> Model
-        this.scene_now.weather_info.fog_type = e.target.selectedIndex > 0;
+        this.client.scene.weather_info.fog_type = e.target.selectedIndex > 0;
     });
 };
 
@@ -166,56 +126,56 @@ DrivingPageUI.prototype.initWeatherUI = function () {
 DrivingPageUI.prototype.initTrafficUI = function () {
     this.m_uiTraffic_carDensity_slider.on(Laya.Event.CHANGED, this, function () {
         // Changed: UI -> Model
-        this.scene_now.traffic_info.car_density = this.m_uiTraffic_carDensity_slider.value;
+        this.client.scene.traffic_info.car_density = this.m_uiTraffic_carDensity_slider.value;
         // Changed: Model -> UI
-        this.refreshTrafficUI();
+        this.client.fire("traffic_info");
     });
     this.m_uiTraffic_carDensity_input.on(Laya.Event.INPUT, this, function (e) {
         // Input: UI -> Model
         var data = parseFloat(e.text);
         if (isFinite(data)) {
-            this.scene_now.traffic_info.car_density = data;
+            this.client.scene.traffic_info.car_density = data;
         }
     });
     this.m_uiTraffic_carDensity_input.on(Laya.Event.BLUR, this, function (e) {
         // Blur: Model -> UI
-        this.refreshTrafficUI();
+        this.client.fire("traffic_info");
     });
 
     this.m_uiTraffic_carIrregularity_slider.on(Laya.Event.CHANGED, this, function () {
         // Changed: UI -> Model
-        this.scene_now.traffic_info.car_irregularity = this.m_uiTraffic_carIrregularity_slider.value;
+        this.client.scene.traffic_info.car_irregularity = this.m_uiTraffic_carIrregularity_slider.value;
         // Changed: Model -> UI
-        this.refreshTrafficUI();
+        this.client.fire("traffic_info");
     });
     this.m_uiTraffic_carIrregularity_input.on(Laya.Event.INPUT, this, function (e) {
         // Input: UI -> Model
         var data = parseFloat(e.text);
         if (isFinite(data)) {
-            this.scene_now.traffic_info.car_irregularity = data;
+            this.client.scene.traffic_info.car_irregularity = data;
         }
     });
     this.m_uiTraffic_carIrregularity_input.on(Laya.Event.BLUR, this, function (e) {
         // Blur: Model -> UI
-        this.refreshTrafficUI();
+        this.client.fire("traffic_info");
     });
 
     this.m_uiTraffic_pedestrainDensity_slider.on(Laya.Event.CHANGED, this, function () {
         // Changed: UI -> Model
-        this.scene_now.traffic_info.pedestrain_density = this.m_uiTraffic_pedestrainDensity_slider.value;
+        this.client.scene.traffic_info.pedestrain_density = this.m_uiTraffic_pedestrainDensity_slider.value;
         // Changed: Model -> UI
-        this.refreshTrafficUI();
+        this.client.fire("traffic_info");
     });
     this.m_uiTraffic_pedestrainDensity_input.on(Laya.Event.INPUT, this, function (e) {
         // Input: UI -> Model
         var data = parseFloat(e.text);
         if (isFinite(data)) {
-            this.scene_now.traffic_info.pedestrain_density = data;
+            this.client.scene.traffic_info.pedestrain_density = data;
         }
     });
     this.m_uiTraffic_pedestrainDensity_input.on(Laya.Event.BLUR, this, function (e) {
         // Blur: Model -> UI
-        this.refreshTrafficUI();
+        this.client.fire("traffic_info");
     });
 };
 
@@ -223,7 +183,7 @@ DrivingPageUI.prototype.initTrafficUI = function () {
 DrivingPageUI.prototype.refreshSceneListUI = function () {
     var data = [];
 
-    this.scene_list.forEach(function (v) {
+    this.client.scene_list.forEach(function (v) {
         data.push({
             label: {
                 text: v.scene_info.scene,
@@ -256,94 +216,21 @@ DrivingPageUI.prototype.refreshPathUI = function () {
 
 // Refresh the weather UI.
 DrivingPageUI.prototype.refreshWeatherUI = function () {
-    this.m_uiWeather_temperature.text = "" + this.scene_now.weather_info.temperature;
-    this.m_uiWeather_timeOfDay.text = "" + this.scene_now.weather_info.time_of_day;
-    this.m_uiWeather_rainType.selectedIndex = this.scene_now.weather_info.rain_type ? 1 : 0;
-    this.m_uiWeather_snowType.selectedIndex = this.scene_now.weather_info.snow_type ? 1 : 0;
-    this.m_uiWeather_fogType.selectedIndex = this.scene_now.weather_info.fog_type ? 1 : 0;
+    this.m_uiWeather_temperature.text = "" + this.client.scene.weather_info.temperature;
+    this.m_uiWeather_timeOfDay.text = "" + this.client.scene.weather_info.time_of_day;
+    this.m_uiWeather_rainType.selectedIndex = this.client.scene.weather_info.rain_type ? 1 : 0;
+    this.m_uiWeather_snowType.selectedIndex = this.client.scene.weather_info.snow_type ? 1 : 0;
+    this.m_uiWeather_fogType.selectedIndex = this.client.scene.weather_info.fog_type ? 1 : 0;
 };
 
 // Refresh the traffic UI.
 DrivingPageUI.prototype.refreshTrafficUI = function () {
-    this.m_uiTraffic_carDensity_slider.value = this.scene_now.traffic_info.car_density;
-    this.m_uiTraffic_carDensity_input.text = "" + this.scene_now.traffic_info.car_density;
-    this.m_uiTraffic_carIrregularity_slider.value = this.scene_now.traffic_info.car_irregularity;
-    this.m_uiTraffic_carIrregularity_input.text = "" + this.scene_now.traffic_info.car_irregularity;
-    this.m_uiTraffic_pedestrainDensity_slider.value = this.scene_now.traffic_info.pedestrain_density
-    this.m_uiTraffic_pedestrainDensity_input.text = "" + this.scene_now.traffic_info.pedestrain_density;
-};
-
-//
-// Controller
-//
-
-DrivingPageUI.prototype.setSceneList = function (scene_list) {
-    if (!Array.isArray(scene_list) || scene_list.length === 0) {
-        throw new Error("Invalid scene list: " + JSON.stringify(scene_list));
-    }
-
-    // Replace the model with a clone of the scene list.
-    this.scene_list = JSON.parse(JSON.stringify(scene_list));
-
-    // Populate the current scene settings.
-    this.setScene(0);
-
-    // Model -> UI
-    this.refreshSceneListUI();
-};
-
-DrivingPageUI.prototype.setScene = function (i) {
-    if (typeof i !== "number" || i < 0 || i >= this.scene_list.length) {
-        throw new Error("Invalid scene index: " + i);
-    }
-
-    // Discard the current scene settings.
-    this.scene_now = JSON.parse(JSON.stringify(this.scene_list[i]));
-
-    // Model -> UI
-    this.refreshPathUI();
-    this.refreshWeatherUI();
-    this.refreshTrafficUI();
-};
-
-//
-// WebSocket Client
-//
-
-// Connect to the backend.
-DrivingPageUI.prototype.initWS = function () {
-    this.socket = new Laya.Socket();
-    this.socket.endian = Laya.Byte.LITTLE_ENDIAN;
-    this.socket.connectByUrl("ws://" + 
-                             (window.location.host || "localhost") +
-                             ":8080");
-
-    // On connection established
-    this.socket.on(Laya.Event.OPEN, this, function (e) {
-        console.info("WebSocket open: " + e.target.url);
-
-        // Claim that this is a Web Client
-        this.socket.send(JSON.stringify({
-            method: "auth",
-            type: 0, // Web Client
-        }));
-    });
-
-    // On message from remote
-    this.socket.on(Laya.Event.MESSAGE, this, function (msg) {
-        console.info(">> " + msg);
-    });
-
-    // On connection close
-    this.socket.on(Laya.Event.CLOSE, this, function (e) {
-        console.info("WebSocket close: " + e.target.url);
-    });
-
-    // On connection error
-    this.socket.on(Laya.Event.ERROR, this, function (e) {
-        // Laya ate the Error ?
-        console.error("WebSocket error: " + e.target.url);
-    });
+    this.m_uiTraffic_carDensity_slider.value = this.client.scene.traffic_info.car_density;
+    this.m_uiTraffic_carDensity_input.text = "" + this.client.scene.traffic_info.car_density;
+    this.m_uiTraffic_carIrregularity_slider.value = this.client.scene.traffic_info.car_irregularity;
+    this.m_uiTraffic_carIrregularity_input.text = "" + this.client.scene.traffic_info.car_irregularity;
+    this.m_uiTraffic_pedestrainDensity_slider.value = this.client.scene.traffic_info.pedestrain_density
+    this.m_uiTraffic_pedestrainDensity_input.text = "" + this.client.scene.traffic_info.pedestrain_density;
 };
 
 Laya.class(DrivingUI, "DrivingUI", DrivingPageUI);
---
+++ src/WebClient.js
@@ -0,0 +1,202 @@
+"use strict";
+
+var WebClient = (function (window, Laya, logger) {
+
+    function WebClient() {
+        this.car_list   = this.getMockCarList();
+        this.car        = JSON.parse(JSON.stringify(this.car_list[0]));
+        this.scene_list = this.getMockSceneList();
+        this.scene      = JSON.parse(JSON.stringify(this.scene_list[0]));
+        this.callbacks  = {};
+        this.init();
+    }
+
+    // Establish the connection to backend node server
+    WebClient.prototype.init = function () {
+        this.socket = new Laya.Socket();
+        this.socket.endian = Laya.Byte.LITTLE_ENDIAN;
+        this.socket.connectByUrl("ws://" + 
+                                (window.location.host || "10.2.10.215") +
+                                ":8080");
+
+        // On connection established
+        this.socket.on(Laya.Event.OPEN, this, function (e) {
+            logger.info("WebSocket open: " + e.target.url);
+
+            // Replace this as soon as scene list is supported
+            if (!this.__init_scene_list) {
+                this.__init_scene_list = true;
+                this.fire("__init_scene_list");
+            }
+
+            // Claim that this is a Web Client
+            this.socket.send(JSON.stringify({
+                method: "auth",
+                type: 0, // Web Client
+            }));
+        });
+
+        // On message from remote
+        this.socket.on(Laya.Event.MESSAGE, this, function (msg) {
+            if (typeof msg === "string") {
+                // Parse the input string into JSON object.
+                var json;
+                try {
+                    json = JSON.parse(msg);
+                } catch (err) {
+                    // Only json is expected. Log and rethrow.
+                    logger.error("WebSocket received invalid json: " + msg);
+                    throw err;
+                }
+
+                // Handle the message from the backend server.
+                this.handleJsonMessage(json);
+            } else {
+                // Only string data is expected.
+                throw new Error("WebSocket received unsupported data.");
+            }
+        });
+
+        // On connection close
+        this.socket.on(Laya.Event.CLOSE, this, function (e) {
+            logger.info("WebSocket close: " + e.target.url);
+        });
+
+        // On connection error
+        this.socket.on(Laya.Event.ERROR, this, function (e) {
+            // Laya ate the Error ?
+            logger.error("WebSocket error: " + e.target.url);
+        });
+    };
+
+    // Handle the JSON message
+    WebClient.prototype.handleJsonMessage = function (json) {
+        logger.info("WebSocket receive: ");
+        logger.info(json);
+
+        // Update the model
+        switch (json.method) {
+
+        case "scene_info":
+            // Only one scene for now. Shouldn't the user choose a scene in browser?
+            Object.assign(this.scene.scene_info, json);
+            break;
+
+        case "weather_info":
+            Object.assign(this.scene.weather_info, json);
+            break;
+
+        case "traffic_info":
+            Object.assign(this.scene.traffic_info, json);
+            break;
+
+        case "car_config":
+            Object.assign(this.car.car_config, json);
+            break;
+
+        }
+
+        // Fire callbacks to refresh the webpage
+        this.fire(json.method);
+    };
+
+    // Add a callback to the client based on method.
+    WebClient.prototype.on = function (method, self, handler) {
+        this.callbacks[method] = this.callbacks[method] || [];
+        this.callbacks[method].push({
+            self: self,
+            handler: handler,
+        });
+        return this;
+    };
+
+    // Fire callbacks based on method.
+    WebClient.prototype.fire = function (method) {
+        if (this.callbacks[method]) {
+            this.callbacks[method].forEach(function (v) {
+                v.handler.apply(v.self);
+            });
+        }
+        return this;
+    };
+
+    // Choose a scene from the list.
+    WebClient.prototype.chooseScene = function (i) {
+        if (typeof i !== "number" || i < 0 || i >= this.scene_list.length) {
+            throw new Error("Invalid scene index: " + i);
+        }
+
+        // Discard the current scene settings.
+        this.scene = JSON.parse(JSON.stringify(this.scene_list[i]));
+
+        // Fire
+        this.fire("scene_info")
+            .fire("weather_info")
+            .fire("traffic_info");
+    };
+
+    // Return a mock car config list for testing
+    WebClient.prototype.getMockCarList = function () {
+        return [
+            {
+                car_config: {
+                    method: "car_config",
+                    config: [
+                        {sid:1, type:1, x:1, y:20, z:0},
+                        {sid:2, type:2, x:0, y:10, z:0},
+                    ],
+                },
+            },
+        ];
+    };
+
+    // Return a mock scene for testing.
+    WebClient.prototype.getMockSceneList = function () {
+        return [
+            {
+                scene_info: {
+                    method: "scene_info",
+                    scene: "IndistrialCity",
+                    path: "default",
+                },
+                weather_info: {
+                    method: "weather_info",
+                    temperature: 25,
+                    time_of_day: "9:00",
+                    rain_type: true,
+                    snow_type: false,
+                    fog_type: "HeaveFog",
+                },
+                traffic_info: {
+                    method: "traffic_info",
+                    car_density: 50,
+                    pedestrain_density: 19,
+                    car_irregularity: 29,
+                },
+            },
+            {
+                scene_info: {
+                    method: "scene_info",
+                    scene: "Changning",
+                    path: "default",
+                },
+                weather_info: {
+                    method: "weather_info",
+                    temperature: -10,
+                    time_of_day: "19:00",
+                    rain_type: false,
+                    snow_type: true,
+                    fog_type: "",
+                },
+                traffic_info: {
+                    method: "traffic_info",
+                    car_density: 500,
+                    pedestrain_density: 109,
+                    car_irregularity: 209,
+                },
+            },
+        ];
+    };
+
+    return WebClient;
+}(window, Laya, console));
