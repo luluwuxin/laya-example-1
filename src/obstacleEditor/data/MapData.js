797e69e4b811c493476efea281b7e63aa89f3094
WangShengyu <wangshengyu@51hitech.com>
2018-01-24 14:28:06 +0800

    Obstacle type are set by a config file.

--- src/obstacleEditor/data/MapData.js
+++ src/obstacleEditor/data/MapData.js
@@ -1,9 +1,26 @@
 class MapData
 {
-    constructor(mapInfo, mapImagePath)
+    constructor(mapConfig, rootDirectory)
     {
-        this.mapInfo = mapInfo;
-        this.mapImagePath = mapImagePath;
+        this.rootDirectory = rootDirectory;
+        ObjectHelper.clone(mapConfig, this);
+
+        // Init.
+        this.obstacleTypes = [];
+        for (var key in this.obstacleInfo)
+        {
+            this.obstacleTypes.push(key);
+        }
+    }
+
+    _getPath(relativePath)
+    {
+        return this.rootDirectory + "/" + relativePath;
+    }
+
+    getMapImagePath()
+    {
+        return this._getPath(this.mapInfo.relativeImagePath);
     }
 
     mapToUIPosition(mapPosition)
@@ -20,5 +37,25 @@ class MapData
         ret.x = uiPosition.x * (this.mapInfo.maxX - this.mapInfo.minX) / this.mapInfo.width + this.mapInfo.minX;
         ret.y = uiPosition.y * (this.mapInfo.maxY - this.mapInfo.minY) / this.mapInfo.height + this.mapInfo.minY;
         return ret;
+    }
+
+    getDefaultObstacleType()
+    {
+        return this.obstacleTypes[0];
+    }
+
+    getObstacleTypes()
+    {
+        return this.obstacleTypes;
+    }
+
+    getObstacleInfo(obstacleType)
+    {
+        return this.obstacleInfo[obstacleType];
+    }
+
+    getObstacleImagePath(obstacleType)
+    {
+        return this._getPath(this.getObstacleInfo(obstacleType).relativeImagePath);
     }
 }
