cbb7a7b4ae930a003d557d8bca02e37deceaa08c
WangShengyu <wangshengyu@51hitech.com>
2018-01-25 19:40:23 +0800

    Now user can select scene when add case.

--- src/webClient/WebClientCase.js
+++ src/webClient/WebClientCase.js
@@ -6,23 +6,81 @@ class WebClientCase
         this.selectedCaseId = -1;
     }
 
+    _getSceneConfig(sceneName)
+    {
+        // TODO: read from file.
+        var cfg = {
+            IndustrialCity: {
+                "mapInfo":
+                {
+                    "relativeImagePath": "assets/map/map.png",
+                    "height": 4384,
+                    "width": 4384,
+                    "minX": -11250,
+                    "maxX": 11250,
+                    "minY": -11250,
+                    "maxY": 11250,
+                    "objectZ": 150
+                },
+                "obstacleInfo":
+                {
+                    "car": {
+                        "relativeImagePath": "assets/obstacle/car.png",
+                        "bpPath": "todo"
+                    },
+                    "man": {
+                        "relativeImagePath": "assets/obstacle/man.png",
+                        "bpPath": "todo"
+                    },
+                    "plane": {
+                        "relativeImagePath": "assets/obstacle/man.png",
+                        "bpPath": "todo"
+                    }
+                }
+            },
+            ParkingLot: {
+                "mapInfo":
+                {
+                    "relativeImagePath": "assets/map/map.png",
+                    "height": 1705,
+                    "width": 5117,
+                    "minX": -6720,
+                    "maxX": 8280,
+                    "minY": -2850,
+                    "maxY": 2150,
+                    "objectZ": 0
+                },
+                "obstacleInfo":
+                {
+                    "car": {
+                        "relativeImagePath": "assets/obstacle/car.png",
+                        "bpPath": "/Game/Blueprints/BP_ParkCar.BP_ParkCar"
+                    }
+                }
+            }
+        }
+        return cfg[sceneName];
+    }
+
     _getCaseId(c)
     {
         return c.name;
     }
 
-    _createCaseName()
+    _createCaseName(sceneName)
     {
         var indexes = new Set();
         var list = this.getList();
+        var prefix = sceneName + "-";
+        var preLen = prefix.length;
         for (var i = 0; i < list.length; i++)
         {
             var name = list[i].name;
-            if (name.substring(0, 5) != "case-")
+            if (name.substring(0, preLen) != prefix)
             {
                 continue;
             }
-            var num = Number(name.substring(5));
+            var num = Number(name.substring(preLen));
             if (isNaN(num) == false)
             {
                 indexes.add(num);
@@ -34,7 +92,7 @@ class WebClientCase
             {
                 continue;
             }
-            return "case-" + i;
+            return prefix + i;
         }
     }
 
@@ -99,13 +157,13 @@ class WebClientCase
         }
     }
 
-    insertDefault()
+    insertDefault(sceneName = "ParkingLot")
     {
         var ret = {};
-        ret.scene = "IndustrialCity";
-        ret.name = this._createCaseName();
+        ret.scene = sceneName;
+        ret.name = this._createCaseName(sceneName);
         ret.content = '{"obstacles":[]}';
-        ret.scene_config = '{"mapInfo":{"relativeImagePath":"assets/map/map.png","height":4384,"width":4384,"minX":-11250,"maxX":11250,"minY":-11250,"maxY":11250,"objectZ":150},"obstacleInfo":{"car":{"relativeImagePath":"assets/obstacle/car.png","bpPath":"/Game/Vehicles/Vehicle001BP.Vehicle001BP"},"man":{"relativeImagePath":"assets/obstacle/man.png","bpPath":"/Game/AI/Walker/BP_Walker_01.BP_Walker_01"}}}';
+        ret.scene_config = this._getSceneConfig(sceneName);
         this.insert(ret);
     }
 
