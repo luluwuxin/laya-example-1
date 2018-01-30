40d0380d004171069c45def6f0906136f394b30a
mingxiang <xumingxiang@51hitech.com>
2018-01-30 14:47:24 +0800

    gulpt and uglify-es in place in release/web

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp   = require("gulp");
+const uglify = require("gulp-uglify-es").default;
+ 
+gulp.task("uglify-web", () => {
+    return gulp.src("release/web/js/**/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("release/web/js/"));
+});
